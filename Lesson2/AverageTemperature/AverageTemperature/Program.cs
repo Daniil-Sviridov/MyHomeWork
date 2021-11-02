5b456edbb8eb95d3e7f7ef05180297a182b28964
Daniil Sviridov <ROSSVIRID@CORP.JTI.COM>
2021-11-02 23:00:26 +0300

    Пункт1

---
+++ Lesson2/AverageTemperature/AverageTemperature/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AverageTemperature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите данные!");
+            Console.WriteLine("Минимальная температура? :");
+            double minTemp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Минимальная температура? :");
+            double maxTemp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Средняя температура  {(minTemp + maxTemp) / 2}");
+
+
+
+        }
+    }
+}

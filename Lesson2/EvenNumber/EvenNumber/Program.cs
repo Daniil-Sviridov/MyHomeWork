9cc84071bdf96cce3cb17421e3b6b852e48aa474
Daniil-Sviridov <93223518+Daniil-Sviridov@users.noreply.github.com>
2021-11-03 01:00:04 +0300

    Четное число

---
+++ Lesson2/EvenNumber/EvenNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = 0;
+            string str = "";
+
+            try
+            {
+                str = Console.ReadLine();
+                Num = Convert.ToInt32(str);
+            }
+            catch 
+            {
+                Console.WriteLine($"Ошибка преобразования к числовому значению: {str}");
+                Console.ReadKey();
+                return;
+            };
+
+            Console.Clear();
+            Console.Write($"Введеное число {Num} четное: ");
+
+            if (Num % 2 == 0) 
+                {
+                Console.Write("Ага");
+            } 
+            else 
+                {
+                Console.Write("Неа");
+                };
+
+            Console.ReadKey();
+            
+        }
+    }
+}

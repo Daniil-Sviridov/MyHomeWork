9bc2184267601f88c2acbbb75559ba64cf92c4d1
Daniil Sviridov <ROSSVIRID@CORP.JTI.COM>
2021-11-11 20:51:13 +0300

    Фибонач

---
+++ Lesson4/L4TM_Fib/L4TM_Fib/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace L4TM_Fib
+{
+    class Program
+    {
+        static int FibNum(int n)
+        {
+            if (n == 0 | n == 1) return n;
+
+            return FibNum(n - 1) + FibNum(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Размер последовательности:");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < Num; i++)
+            Console.WriteLine($"{i+1} {FibNum(i)}");
+
+            Console.ReadKey();
+        }
+    }
+}

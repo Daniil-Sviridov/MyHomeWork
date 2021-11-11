cdf7a3ee78d07fdc41e295ae8145cce298e4c8f9
Daniil Sviridov <ROSSVIRID@CORP.JTI.COM>
2021-11-12 01:51:10 +0300

    Task2

---
+++ Lesson5/L5T2_AddDateToFile/L5T2_AddDateToFile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace L5T2_AddDateToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime Dat =  DateTime.Now;
+
+            File.AppendAllText("startup.txt", $"\n  {Dat.TimeOfDay.ToString()}");            
+        }
+    }
+}

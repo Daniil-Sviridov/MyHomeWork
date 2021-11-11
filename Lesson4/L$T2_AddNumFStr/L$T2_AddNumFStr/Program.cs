5ad4e2718b4693ff8c68aedad73b5859ed6e04a8
Daniil Sviridov <ROSSVIRID@CORP.JTI.COM>
2021-11-11 18:37:28 +0300

    L_T2_AddNumFStr

---
+++ Lesson4/L$T2_AddNumFStr/L$T2_AddNumFStr/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace L_T2_AddNumFStr
+{
+    
+
+    class Program
+    {
+        static int Sum(params int[] values)
+        {
+            int sum = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                sum += values[i];
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите целые числа через пробел:");
+            string Str = Console.ReadLine();
+            Console.WriteLine("Сумма:");
+            Console.WriteLine(Sum(Str.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray()));
+            
+            Console.ReadKey();
+        }
+    }
+}

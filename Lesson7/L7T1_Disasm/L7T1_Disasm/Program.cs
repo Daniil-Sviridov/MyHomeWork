9ea2a89b2f7627df6ddad33a9495928873744aa5
Daniil Sviridov <ROSSVIRID@CORP.JTI.COM>
2021-11-19 17:26:18 +0300

    пушим

---
+++ Lesson7/L7T1_Disasm/L7T1_Disasm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace L7T1_Disasm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string s in args )
+                {
+                Console.WriteLine(s);
+                };
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}

331f83aec37ab1042ac1feca0b845b5b67395598
Daniil-Sviridov <93223518+Daniil-Sviridov@users.noreply.github.com>
2021-11-20 20:52:30 +0300

    Ура!!

---
+++ Lesson7/L7T2_Disasm/L6_TaskMgr/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+[assembly: AssemblyCompany("L6_TaskMgr")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("L6_TaskMgr")]
+[assembly: AssemblyTitle("L6_TaskMgr")]
+[assembly: AssemblyVersion("1.0.0.0")]
